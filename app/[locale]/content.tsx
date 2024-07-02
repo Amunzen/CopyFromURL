5e5b474e42b8a5bb00a4f18ffbd3b2db88168ff5
Keisuke Nagakawa <kay@amunzen.com>
2024-07-02 23:34:15 +0900

    Replace native clipboard API with `copy-to-clipboard` library to improve cross-browser compatibility

--- app/[locale]/content.tsx
+++ app/[locale]/content.tsx
@@ -15,6 +15,7 @@ import { Examples } from "./example"
 import { useInitFocus } from "./hooks"
 import { URLInput } from "./input"
 import { handleFetchApiRoute } from "./utils"
+import copy from "copy-to-clipboard"
 
 
 
@@ -47,7 +48,8 @@ export function Content({ locale }: { locale: Locale }) {
       }
       const text = (response as { text: string }).text
       setText(text)
-      await navigator.clipboard.writeText(text)
+      // await navigator.clipboard.writeText(text)
+      copy(text)
       setFetchedAndCopied(true)
       toast({
         title: t("copied"),
@@ -55,89 +57,89 @@ export function Content({ locale }: { locale: Locale }) {
         duration: 4000,
         variant: "success"
       })
-    setTimeout(() => setFetchedAndCopied(false), 1200)
-  } catch (e) {
-    console.error(e)
-    setError(t("unknownError"))
-    setText("")
-  } finally {
-    setIsLoading(false)
-  }
-}
-
-return <form className="flex flex-col gap-5" onSubmit={handleFetch}>
-  <div className="relative flex items-center">
-    <URLInput
-      disabled={isLoading}
-      url={url}
-      onChange={(e) => setUrl(e.target.value)}
-      ref={inputRef}
-    />
-    <LinkIcon className="absolute left-3 text-gray-400" size={20} />
-    <div className="absolute right-2 flex space-x-2 items-center">
-      <Popover excludeTags={excludeTags} includeTags={includeTags} onExcludeTagsChange={setExcludeTags} onIncludeTagsChange={setIncludeTags} />
-      <GradientButton
-        type="submit"
-        disabled={isLoading || fetchedAndCopied}
-        isLoading={isLoading}
-      >
-        {!fetchedAndCopied ? "Go!" : <>
-          <CheckIcon className="w-4 h-4" />
-          {t("copied")}
-        </>}
-      </GradientButton>
-    </div>
-  </div>
-  {error &&
-    <Alert variant={"destructive"}>
-      <ExclamationTriangleIcon className="h-4 w-4" />
-      <AlertTitle className="text-red-500">
-        {t("error")}
-      </AlertTitle>
-      <AlertDescription>
-        {error}
-      </AlertDescription>
-    </Alert>
-  }
-  <Examples onClick={(url) => setUrl(url)} isLoading={isLoading} />
-  <div className="relative">
-    <Textarea
-      placeholder={t("textareaPlaceholder")}
-      value={text}
-      disabled={!text}
-      onChange={(e) => setText(e.target.value)}
-      className="h-64 p-4 border-2 border-gray-300 focus:border-blue-500 transition-colors rounded-3xl"
-    />
-
-    {
-      text &&
-      <Button
-        variant={"ghost"}
-        type="button"
-        onClick={() => {
-          navigator.clipboard.writeText(text)
-          setCopied(true)
-          setTimeout(() => setCopied(false), 1200)
-        }}
-        disabled={!text}
-        className="absolute right-2 bottom-2  bg-white hover:bg-white"
-
-      >
-        <div className="flex items-center gap-2 text-foreground/70 text-sm">
-          {copied ?
-            <>
-              <CheckIcon className="w-3 h-3" />
-              <span>{t("copied")}</span>
-            </> :
-            <>
-              <CopyIcon className="w-3 h-3" />
-              <span>{t("copy")}</span>
-            </>
-          }
-        </div>
-      </Button>
+      setTimeout(() => setFetchedAndCopied(false), 1200)
+    } catch (e) {
+      console.error(e)
+      setError(t("unknownError"))
+      setText("")
+    } finally {
+      setIsLoading(false)
     }
-  </div>
-</form>
+  }
+
+  return <form className="flex flex-col gap-5" onSubmit={handleFetch}>
+    <div className="relative flex items-center">
+      <URLInput
+        disabled={isLoading}
+        url={url}
+        onChange={(e) => setUrl(e.target.value)}
+        ref={inputRef}
+      />
+      <LinkIcon className="absolute left-3 text-gray-400" size={20} />
+      <div className="absolute right-2 flex space-x-2 items-center">
+        <Popover excludeTags={excludeTags} includeTags={includeTags} onExcludeTagsChange={setExcludeTags} onIncludeTagsChange={setIncludeTags} />
+        <GradientButton
+          type="submit"
+          disabled={isLoading || fetchedAndCopied}
+          isLoading={isLoading}
+        >
+          {!fetchedAndCopied ? "Go!" : <>
+            <CheckIcon className="w-4 h-4" />
+            {t("copied")}
+          </>}
+        </GradientButton>
+      </div>
+    </div>
+    {error &&
+      <Alert variant={"destructive"}>
+        <ExclamationTriangleIcon className="h-4 w-4" />
+        <AlertTitle className="text-red-500">
+          {t("error")}
+        </AlertTitle>
+        <AlertDescription>
+          {error}
+        </AlertDescription>
+      </Alert>
+    }
+    <Examples onClick={(url) => setUrl(url)} isLoading={isLoading} />
+    <div className="relative">
+      <Textarea
+        placeholder={t("textareaPlaceholder")}
+        value={text}
+        disabled={!text}
+        onChange={(e) => setText(e.target.value)}
+        className="h-64 p-4 border-2 border-gray-300 focus:border-blue-500 transition-colors rounded-3xl"
+      />
+
+      {
+        text &&
+        <Button
+          variant={"ghost"}
+          type="button"
+          onClick={() => {
+            copy(text)
+            setCopied(true)
+            setTimeout(() => setCopied(false), 1200)
+          }}
+          disabled={!text}
+          className="absolute right-2 bottom-2  bg-white hover:bg-white"
+
+        >
+          <div className="flex items-center gap-2 text-foreground/70 text-sm">
+            {copied ?
+              <>
+                <CheckIcon className="w-3 h-3" />
+                <span>{t("copied")}</span>
+              </> :
+              <>
+                <CopyIcon className="w-3 h-3" />
+                <span>{t("copy")}</span>
+              </>
+            }
+          </div>
+        </Button>
+      }
+    </div>
+  </form>
 
 }
