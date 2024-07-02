73d361d87d59e0fa71f229bda5297485d7f6ae38
Keisuke Nagakawa <kay@amunzen.com>
2024-07-02 22:43:41 +0900

    Internationalize URL input placeholder using `next-intl` for localization support

--- app/[locale]/input.tsx
+++ app/[locale]/input.tsx
@@ -1,4 +1,5 @@
 import { Input as RawInput } from "@/components/ui/input"
+import { useTranslations } from "next-intl"
 import React from 'react'
 
 type Props = {
@@ -8,13 +9,14 @@ type Props = {
 }
 
 export const URLInput = React.forwardRef<HTMLInputElement, Props>(({ url, disabled, onChange }, ref) => {
+  const t = useTranslations()
   return (
     <RawInput
       disabled={disabled}
       maxLength={256}
       ref={ref}
       type="url"
-      placeholder="URLを入力"
+      placeholder={t("urlPlaceholder")}
       value={url}
       onChange={onChange}
       className="pl-10 pr-24 py-6 text-lg rounded-full border-2 border-gray-300 focus:border-blue-500 transition-colors"
